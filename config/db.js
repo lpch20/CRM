2679db3e2bd837be1f3ef2996cb5a976f976cff5
lpch20 <lpch20@hotmail.com>
2024-04-18 20:34:36 -0300

    intentando de todos modos con vercel

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     const db = await mongoose.connect(
-        process.env.MONGO_URI,
+        process.env.MONGODB_URI,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
